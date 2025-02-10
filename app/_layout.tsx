868324a721a04f9a7edf824b27c4844b3a577774
Derek Scruggs <me@derekscruggs.com>
2025-02-10 10:34:04 -0600

    fetch and render challenges on home screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,34 +1,41 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Tabs } from 'expo-router';
 
 import { TabBarIcon } from '~/components/TabBarIcon';
 
+// Create a client
+const queryClient = new QueryClient();
+
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: 'black',
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="about"
-        options={{
-          href: null,
-        }}
-      />
-    </Tabs>
+    <QueryClientProvider client={queryClient}>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: 'black',
+        }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Tab One',
+            tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="two"
+          options={{
+            title: 'Tab Two',
+            tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="about"
+          options={{
+            title: 'About',
+            tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          }}
+        />
+      </Tabs>
+    </QueryClientProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,62 @@
-import { Stack } from 'expo-router';
+import { Icon } from '@roninoss/icons';
+import { useQuery } from '@tanstack/react-query';
+import { Link, Stack } from 'expo-router';
+import { Platform, View, SafeAreaView, ViewStyle, ScrollView } from 'react-native';
 
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
+import { Button } from '~/components/nativewindui/Button';
+import { Text } from '~/components/nativewindui/Text';
+import { useColorScheme } from '~/lib/useColorScheme';
+
+const ROOT_STYLE: ViewStyle = { flex: 1 };
+async function fetchChallenges() {
+  console.log('fetching challenges');
+  const result = await fetch('http://localhost:3000/api/challenges/active');
+  const data = await result.json();
+  return data;
+}
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['challenges'],
+    queryFn: fetchChallenges,
+  });
+  const { colors } = useColorScheme();
+  if (isLoading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error loading challenges</Text>;
+
+  // Ensure data is an array before mapping
+  const challenges = Array.isArray(data.challenges) ? data.challenges : [];
+  console.log('challenges', challenges);
   return (
-    <>
-      <Stack.Screen options={{ title: 'Tab One' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Tab One" />
-      </Container>
-    </>
+    <SafeAreaView style={ROOT_STYLE}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View className="mx-auto w-full flex-1 justify-between gap-4 border border-red-500 px-2 py-4">
+          <View className="ios:pt-8 pt-12">
+            <Text
+              variant="largeTitle"
+              className="ios:text-left ios:font-black text-center font-bold">
+              Welcome to
+            </Text>
+            <Text
+              variant="largeTitle"
+              className="ios:text-left ios:font-black text-center font-bold text-primary">
+              Trybe
+            </Text>
+          </View>
+          <View className="gap-8">
+            {challenges.map((challenge: any) => (
+              <View key={challenge.id} className="flex-row gap-4">
+                <View className="flex-1">
+                  <Text className="font-bold">{challenge.name}</Text>
+                  <Text variant="footnote">{challenge.description}</Text>
+                </View>
+              </View>
+            ))}
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
